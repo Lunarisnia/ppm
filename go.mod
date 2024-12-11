ab36c654db1b38879cccb5d5b114f0be880edc75
Rio Arswendo Rachmad <r.arswendo.r@gmail.com>
2024-12-11 18:00:18 +0700

    feat: added basic ppm utility

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Lunarisnia/ppm
+
+go 1.23.2
+
+require github.com/Lunarisnia/vecm v0.0.0-20241211090152-60d9e61b690f // indirect
---
+++ ppm.go
@@ -0,0 +1,41 @@
+package ppm
+
+import (
+	"fmt"
+
+	"github.com/Lunarisnia/vecm"
+)
+
+type PPM struct {
+	Width       int
+	Height      int
+	AspectRatio float64
+	pixelIndex  int
+	imageSize   int
+}
+
+func CreateImage(width int, height int) *PPM {
+	fmt.Println("P3")
+	fmt.Printf("%v %v\n", width, height)
+	fmt.Println(255)
+	return &PPM{
+		Width:       width,
+		Height:      height,
+		AspectRatio: float64(width) / float64(height),
+		imageSize:   width * height,
+	}
+}
+
+func (p *PPM) Draw(color vecm.Vector3f) {
+	if p.pixelIndex <= p.imageSize {
+		return
+	}
+	c := vecm.Vector3f{
+		X: color.X * 255.0,
+		Y: color.Y * 255.0,
+		Z: color.Z * 255.0,
+	}
+
+	fmt.Printf("%v %v %v\n", c.X, c.Y, c.Z)
+	p.pixelIndex++
+}
